529e261c79ebb6077a84703587994fe9a4a599c4
ama2744 <ama2744@rit.edu>
2017-05-10 15:58:55 -0400

    Update SpeakingRequestManager.java

--- SpeakingRequestManager.java
+++ SpeakingRequestManager.java
@@ -7,49 +7,124 @@ public class SpeakingRequestManager{
       db = new ResearchDb();
    }
    
-   public boolean addRequest(String name, String email, ArrayList<String> recipients, String msg){
-      boolean success = true;
-      
-      //for each author
-      
-         //start transaction
-         
-         //get author's faculty id
-         
-         //create query to insert just name, email, msg, facId fields
-         
-         //call db setData with query and ArrayList of values
-         
-         //end transaction
-         
-      return success;
-   }
-   
-   public boolean addRequest(String name, String email, String phone, ArrayList<String> recipients, String msg){
-      boolean success = true;
-      
-      //for each author
-      
-         //start transaction
-         
-         //get author's faculty id
-         
-         //create query to insert just name, email, msg, facId fields
-         
-         //call db setData with query and ArrayList of values
-         
-         //end transaction
-         
-      return success; 
-   }
-   
-   public ArrayList<SpeakingRequest> getRequestsByFaculty(int facId){
-      //call db getData to retrieve speaking requests for the given faculty id
-      
-      //go through returned data, creating an ArrayList of SpeakingRequests
-      
-      //CHANGE THIS TO RETURN ARRAYLIST WHEN COMPLETE
-      return null;
-   }
+	public boolean addRequest(String name, String email, ArrayList<String> recipients, String msg) {
+		boolean success = true;
+		String fID;
 
+		ArrayList<String> authors = new ArrayList<String>();
+		ArrayList<String> request = new ArrayList<String>();
+
+		// create query to insert just name, email, msg, facId fields
+		String query1 = "insert into speaking_requests (requesterName, email, phone, request,facultyId) values(?,?,?,?,?)";
+		String query2 = "SELECT id FROM faculty WHERE CONCAT(fName,' ',lName) = ?";
+
+		request.add(name);
+		request.add(email);
+		request.add("");
+		request.add(msg);
+
+		try {
+
+			// start transaction
+			db.startTrans();
+			// for each author
+			for (String recipient : recipients) {
+
+				// get author's faculty id
+				authors.add(recipient);
+				fID = db.getData(query2, authors, false).get(0).get(0);
+
+				request.add(fID);
+
+				// call db setData with query and ArrayList of values
+				db.setData(query1, request);
+
+			}
+			// end transaction
+			db.endTrans();
+		} catch (DLException e) {
+			try {
+				db.rollbackTrans();
+			} catch (DLException e1) {
+				e1.printStackTrace();
+			}
+			e.printStackTrace();
+			success = false;
+		}
+
+		return success;
+	}
+   
+	public boolean addRequest(String name, String email, String phone, ArrayList<String> recipients, String msg) {
+		boolean success = true;
+		String fID;
+
+		ArrayList<String> authors = new ArrayList<String>();
+		ArrayList<String> request = new ArrayList<String>();
+
+		// create query to insert just name, email, msg, facId fields
+		String query1 = "insert into speaking_requests (requesterName, email, phone, request,facultyId) values(?,?,?,?,?)";
+		String query2 = "SELECT id FROM faculty WHERE CONCAT(fName,' ',lName) = ?";
+
+		request.add(name);
+		request.add(email);
+		request.add(phone);
+		request.add(msg);
+
+		try {
+
+			// start transaction
+			//db.startTrans();
+			// for each author
+			for (String recipient : recipients) {
+				// get author's faculty id
+				authors.add(recipient);
+				fID = db.getData(query2, authors, false).get(0).get(0);
+
+				request.add(fID);
+
+				// call db setData with query and ArrayList of values
+				db.setData(query1, request);
+				request.remove(4);
+				authors.remove(0);
+			}
+			// end transaction
+			//db.endTrans();
+		} catch (DLException e) {
+//			try {
+//				db.rollbackTrans();
+//			} catch (DLException e1) {
+//				e1.printStackTrace();
+//			}
+			e.printStackTrace();
+			success = false;
+		}
+
+		return success;
+	}
+   
+	public ArrayList<SpeakingRequest> getRequestsByFaculty(int facId) {
+
+		String query = "select * from speaking_requests where facultyId = ?";
+		ArrayList<String> id = new ArrayList<String>();
+		ArrayList<ArrayList<String>> speakingRequests = new ArrayList<ArrayList<String>>();
+		ArrayList<SpeakingRequest> speakingRequestsList = new ArrayList<SpeakingRequest>();
+		id.add(Integer.toString(facId));
+
+		try {
+			// call db getData to retrieve speaking requests for the given faculty
+			speakingRequests = db.getData(query, id, false);
+			// go through returned data, creating an ArrayList of SpeakingRequests
+			for (ArrayList<String> arrayList : speakingRequests) {
+				SpeakingRequest speakingRequest = new SpeakingRequest(arrayList.get(1), arrayList.get(2),
+						arrayList.get(3));
+				speakingRequestsList.add(speakingRequest);
+			}
+
+		} catch (DLException e) {
+			e.printStackTrace();
+		}
+
+		return speakingRequestsList;
+	}
 }
